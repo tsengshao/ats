0c86dc282d02781e27f845a2475614b996987363
Shao-Yu <shaoyu.ts@gmail.com>
2025-05-05 10:02:10 +0800

    modify utils, split taiwan map

--- utils/utils_plot_cartopy.py
+++ utils/utils_plot_cartopy.py
@@ -23,11 +23,11 @@ class PlotTools_cartopy():
             if grid_info['xloc_'] is not None:
                 axe.set_xticks(grid_info['xloc_'], crs=self.proj)
                 #axe.set_xticklabels(['' for i in range(len(grid_info['xloc_']))])  # default: no tick labels
-                axe.set_xticklabels([f'{int(i)}E' for i in grid_info['xloc_']])
+                axe.set_xticklabels([f'{int(i)}E' for i in grid_info['xloc_']],fontsize=16)
             if grid_info['yloc_'] is not None:
                 axe.set_yticks(grid_info['yloc_'], crs=self.proj)
                 #axe.set_yticklabels(['' for i in range(len(grid_info['yloc_']))])
-                axe.set_yticklabels([f'{int(i)}N' for i in grid_info['yloc_']])
+                axe.set_yticklabels([f'{int(i)}N' for i in grid_info['yloc_']],fontsize=16)
             gl = axe.gridlines(xlocs=grid_info['xloc_'], ylocs=grid_info['yloc_'],
                                draw_labels=False)
         return axe
@@ -67,6 +67,35 @@ class PlotTools_cartopy():
                             cmap=cmap, norm=norm, alpha=alpha, edgecolors=edgecolors)
         return im
 
+def create_taiwan_axes():
+    # Figure initialize
+    #fig = plt.figure(figsize=(4, 8))
+    #gs  = mpl.gridspec.GridSpec(1, 1, figure=fig)
+    #fig, ax = plt.subplots(figsize=(4,8))
+    fig = plt.figure(figsize=(4,8))
+    ## Plot:
+    plottools = PlotTools_cartopy()
+    ax1   = plottools.Axe_map(fig, 111, xlim_=[119.95, 122.05], ylim_=[21.85, 25.5],
+                              xloc_=np.arange(120, 122.1, 1), yloc_=np.arange(22, 25.1, 1))
+    plottools.Plot_vvm_map(ax1, '0.5', 1.5)  # coastline
+    # temp = np.argsort(precip_array)
+    # imcwb = plottools.Plot_twcwb_pcp(ax1, stn_lon[temp], stn_lat[temp], precip_array[temp], s=75, alpha=alpha, precip_bounds=bounds)
+    cax0  = fig.add_axes([ax1.get_position().x1+0.06, ax1.get_position().y0, 0.03, ax1.get_position().height])
+    # cbar = fig.colorbar(imcwb, orientation='vertical', cax=cax)
+    # cbar.solids.set(alpha=1)  # Default: set cbar to full color (w/out tranparency)
+    # cbar.set_ticks(ticks=bounds, labels=[int(i) for i in bounds])
+    # cbar.set_label('mm/day', fontsize=16)
+    # cbar.ax.tick_params(labelsize=13)
+
+    topo_bounds= np.arange(0, 3500.1, 500)
+    imtopoh    = plottools.Plot_vvm_topo(ax1, '0.5', 0.5)
+    cax_topo  = fig.add_axes([ax1.get_position().x0,ax1.get_position().y0-0.06, ax1.get_position().width, 0.015])
+    cbar = fig.colorbar(imtopoh, orientation='horizontal', cax=cax_topo, extend='max')
+    cbar.set_ticks(ticks=topo_bounds, labels=[f"{i/1e3:.1f}" for i in topo_bounds])
+    cbar.ax.tick_params(labelsize=13)
+    cbar.set_label('Topography Height (km)', fontsize=16)
+    cbar.outline.set_linewidth(1.5)
+    return plottools, fig, ax1, cax0, cax_topo
 
 
 def Plot_cwb_comp(rainfall_label:str, stn_lon, stn_lat, precip_array, figtitle=False, figname=False):
@@ -79,15 +108,11 @@ def Plot_cwb_comp(rainfall_label:str, stn_lon, stn_lat, precip_array, figtitle=F
     # Figure initialize
     #fig = plt.figure(figsize=(4, 8))
     #gs  = mpl.gridspec.GridSpec(1, 1, figure=fig)
-    fig, ax = plt.subplots(figsize=(4,8))
-    ## Plot:
-    plottools = PlotTools_cartopy()
-    ax1   = plottools.Axe_map(fig, ax, xlim_=[119.95, 122.05], ylim_=[21.85, 25.5],
-                              xloc_=np.arange(120, 122.1, 1), yloc_=np.arange(22, 25.1, 1))
-    plottools.Plot_vvm_map(ax1, '0.5', 1.5)  # coastline
+    plottools, fig, ax, cax, cax_topo = create_taiwan_axes()
+
     temp = np.argsort(precip_array)
-    imcwb = plottools.Plot_twcwb_pcp(ax1, stn_lon[temp], stn_lat[temp], precip_array[temp], s=75, alpha=alpha, precip_bounds=bounds)
-    cax  = fig.add_axes([ax1.get_position().x1+0.06, ax1.get_position().y0, 0.03, ax1.get_position().height])
+    imcwb = plottools.Plot_twcwb_pcp(ax, stn_lon[temp], stn_lat[temp], precip_array[temp], s=75, alpha=alpha, precip_bounds=bounds)
+
     cbar = fig.colorbar(imcwb, orientation='vertical', cax=cax)
     cbar.solids.set(alpha=1)  # Default: set cbar to full color (w/out tranparency)
     cbar.set_ticks(ticks=bounds, labels=[int(i) for i in bounds])
@@ -95,21 +120,10 @@ def Plot_cwb_comp(rainfall_label:str, stn_lon, stn_lat, precip_array, figtitle=F
     cbar.ax.tick_params(labelsize=13)
     cbar.outline.set_linewidth(1.5)
 
-    topo_bounds= np.arange(0, 3500.1, 500)
-    imtopoh    = plottools.Plot_vvm_topo(ax1, '0.5', 0.5)
-    cax  = fig.add_axes([ax1.get_position().x0,ax1.get_position().y0-0.06, ax1.get_position().width, 0.015])
-    cbar = fig.colorbar(imtopoh, orientation='horizontal', cax=cax, extend='max')
-    cbar.set_ticks(ticks=topo_bounds, labels=[f"{i/1e3:.1f}" for i in topo_bounds])
-    cbar.ax.tick_params(labelsize=13)
-    cbar.set_label('Topography Height (km)', fontsize=16)
-    cbar.outline.set_linewidth(1.5)
-
-    ax1.set_xticklabels([f'{int(i)}E' for i in np.arange(120, 122.1, 1)], fontsize=16)
-    ax1.set_yticklabels([f'{int(i)}N' for i in np.arange(22, 25.1, 1)], fontsize=16)
     if figtitle:
-        ax1.set_title(f'{figtitle}', loc='left', fontsize=20)
+        ax.set_title(f'{figtitle}', loc='left', fontsize=20)
     else:
-        ax1.set_title('CWA precipitation', fontsize=16)
+        ax.set_title('CWA precipitation', fontsize=16)
     if figname:
         plt.savefig(f'./fig/{figname}.png', facecolor='w', bbox_inches='tight', dpi=400)
     else:

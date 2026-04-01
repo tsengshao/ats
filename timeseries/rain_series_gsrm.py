c17654e42867099e12e7fbb48dbde2a0705ef681
shao <d12229003@ntu.edu.tw>
2026-04-02 00:08:05 +0900

    modify composite rain_series_gsrm.py

--- timeseries/rain_series_gsrm.py
+++ timeseries/rain_series_gsrm.py
@@ -10,20 +10,30 @@ import matplotlib.pyplot as plt
 lonb = [119.5, 122.5]
 latb = [21.5, 26]
 
+model = 'icon'
+model = 'nicam'
+
+## ds = pd.read_csv(f'../obs/shao_ATdays_2020_2020_{model}.txt', header=None)
+## ds = pd.to_datetime(ds[0])
+
+## df = pd.read_csv(f'../synoptic/csv/{model}_2020.csv',
+##                  usecols=['time', 'wtype', 'diurnal_rain']
+##                 )
+## iloc = (df['wtype']=='other') * (df['diurnal_rain']==True)
+## ds = pd.to_datetime( df['time'][iloc].reset_index(drop='index') )
+
+ds = uread.read_selected_date(model)
+
+ndays = ds.size
+
 # find tw_mask
-lon, lat, landfrac = uread.read_gsrm('nicam', 'sftlf', datetime(1970,1,1), lonb, latb)
+lon, lat, landfrac = uread.read_gsrm(model, 'sftlf', datetime(1970,1,1), lonb, latb)
 lon2d, lat2d = np.meshgrid(lon,lat)
 condi  = (120<=lon2d) * (lon2d<=122.0) *\
          (21.5<=lat2d) * (lat2d<=25.4) *\
          (landfrac>0.2)
 tw_mask = np.where(condi,True,False)
 tw_idx  = np.nonzero(tw_mask)
-
-model = 'icon'
-#model = 'nicam'
-ds = pd.read_csv(f'../obs/shao_ATdays_2020_2020_{model}.txt', header=None)
-ds = pd.to_datetime(ds[0])
-ndays = ds.size
 
 diurnal_array = np.zeros((24,ndays))
 for iday in range(ndays):
@@ -35,7 +45,7 @@ for iday in range(ndays):
 
 udraw.set_figure_defalut()
 fig, ax = plt.subplots(figsize=(6,5.5))
-x = np.arange(0,24)
+x = np.arange(1,25)
 for i in range(diurnal_array.shape[1]):
     plt.plot(x, diurnal_array[:,i], lw=2, c='0.8')
 plt.plot(x, diurnal_array.mean(axis=1), lw=8, c='k')
@@ -48,6 +58,7 @@ plt.xlabel('Time (h)',fontsize=20)
 plt.ylabel('precip. (mm h'+'$^{-1}$'+')', fontsize=20)
 plt.title(f'{model.upper()}', fontsize=20, fontweight='bold')
 plt.title(f'{ndays:d}', fontsize=15, fontweight='bold', loc='right')
+os.makedirs('./fig', exist_ok=True)
 plt.savefig(f'./fig/series_{model}.png', dpi=250, bbox_inches='tight')
 
 
